595786a94b972d3ba18a91ced249834a35695656
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2018-11-23 18:57:12 +0200

    meeting broadcasts page

---
+++ Home/MeetingController.js
@@ -0,0 +1,14 @@
+﻿var myCtrl = ['$scope', 'AngularServices', function ($scope, AngularServices) {
+
+    angular.element(document).ready(function () {
+        
+
+    });
+
+    $("#btnMeeting").click(function () {
+        Redirect("Main.html");
+    });
+
+}];
+
+app.controller("myCtrl", myCtrl);
