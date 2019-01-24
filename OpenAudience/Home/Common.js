3044c08aa1aac00c0e07adee87537088d34b5ee5
amiraelmahdaly <amira.elmahdaly@yahoo.com>
2019-01-24 12:45:29 +0200

    switching open audience to production

--- OpenAudience/Home/Common.js
+++ OpenAudience/Home/Common.js
@@ -27,9 +27,11 @@ app.filter('encodeURIComponent', function () {
 //var subDomain = "app";
 //var BaseAPIURI = "https://" + subDomain + ".meet.ps/api/";
 //var BaseURL = "https://" + subDomain + ".meet.ps/";
-var subDomain = "dev";
-var BaseAPIURI = "https://" + subDomain + ".openmeet.eu/api/";
-var BaseURL = "https://" + subDomain + ".openmeet.eu/";
+//var subDomain = "dev";
+//var BaseAPIURI = "https://" + subDomain + ".openmeet.eu/api/";
+//var BaseURL = "https://" + subDomain + ".openmeet.eu/";
+var BaseAPIURI = "https://openmeet.eu/api/";
+var BaseURL = "https://openmeet.eu/";
 
 // Error Handling Region
 //$(document).ready(function () {
