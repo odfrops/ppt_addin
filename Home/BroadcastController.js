f9cb57119dbbcca85210b964710cc24a055aacfc
odfrops <dofrops@gmail.com>
2021-04-13 10:18:56 -0400

    change setInterval function

--- Home/BroadcastController.js
+++ Home/BroadcastController.js
@@ -64,44 +64,43 @@
 
         }
     }
-
-    function Recursive(SlideID, BroadcastID, MeetingID) {
-        Office.context.document.getActiveViewAsync(function (asyncResult) {
-            if (asyncResult.status == "failed") {
-                console.log("Action failed with error: " + asyncResult.error.message);
-            }
-            else {
-                if (asyncResult.value == 'read')
-                    Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange, function (r) {
-                        if (r.status != "failed") {
-                            if (SlideID == r.value.slides[0].id) {
-
-                                StartBroadcast(MeetingID, BroadcastID);
-                            }
-                            else {
-                                EndBroadcast(MeetingID, BroadcastID);
-                            }
-                        }
-
-
-                    });
-                else {
-                    EndBroadcast(MeetingID, BroadcastID);
+    function refreshBroadcast(SlideID, BroadcastID, MeetingID) {
+        var a = setInterval((function (SlideID, BroadcastID, MeetingID) {
+            Office.context.document.getActiveViewAsync(function (asyncResult) {
+                if (asyncResult.status == "failed") {
+                    console.log("Action failed with error: " + asyncResult.error.message);
                 }
-            }
-        });
-}
+                else {
+                    if (asyncResult.value == 'read')
+                        Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange, function (r) {
+                            if (r.status != "failed") {
+                                if (SlideID == r.value.slides[0].id) {
+    
+                                    StartBroadcast(MeetingID, BroadcastID);
+                                }
+                                else {
+                                    EndBroadcast(MeetingID, BroadcastID);
+                                }
+                            }
+    
+    
+                        });
+                    else {
+                        EndBroadcast(MeetingID, BroadcastID);
+                    }
+                }
+            });
+            clearInterval(a);
+            refreshBroadcast(SlideID, BroadcastID, MeetingID);
+        })(SlideID, BroadcastID, MeetingID), 1000);
+    }
 
     function Begin() {
         var SlideID = Office.context.document.settings.get('SlideID');
         var BroadcastID = Office.context.document.settings.get('BroadcastID');
         var MeetingID = Office.context.document.settings.get('MeetingID');
         if (BroadcastID != null) {
-            setInterval((function (SlideID, BroadcastID, MeetingID) {
-                return function () {
-                    Recursive(SlideID, BroadcastID, MeetingID);
-                };
-            })(SlideID, BroadcastID, MeetingID), 1000);
+            refreshBroadcast(SlideID, BroadcastID, MeetingID);
         }
     }
     $scope.RedirectToMeetings = function () {
